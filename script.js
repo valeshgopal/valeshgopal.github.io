f1fd557a664adb062d7ef5c07a19da83a28e2465
Valesh Gopal <valesh.dev@gmail.com>
2022-10-05 11:49:46 +0530

    close mobile menu nav on click

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-const btn = document.getElementById("menu-btn");
-const menu = document.getElementById("menu");
-const nav = document.querySelector(".nav");
-const hero = document.querySelector(".section-hero");
-const sectionAbout = document.querySelector(".about");
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const nav = document.querySelector('.nav');
+const hero = document.querySelector('.section-hero');
+const sectionAbout = document.querySelector('.about');
 
-btn.addEventListener("click", () => {
-  btn.classList.toggle("open");
-  menu.classList.toggle("hidden");
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open');
+  menu.classList.toggle('hidden');
 });
 
 const stickyNav = function (entries) {
   const [entry] = entries;
   if (!entry.isIntersecting) {
-    nav.classList.add("sticky");
+    nav.classList.add('sticky');
   } else {
-    nav.classList.remove("sticky");
+    nav.classList.remove('sticky');
   }
 };
 
 const obsOptions = {
   root: null,
   threshold: 0,
-  rootMargin: "96px",
+  rootMargin: '96px',
 };
 
 const heroObserver = new IntersectionObserver(stickyNav, obsOptions);
 heroObserver.observe(hero);
+
+const mobileMenuLinks = document.querySelectorAll('.mobile-menu-link');
+mobileMenuLinks.forEach((mobileMenuLink) => {
+  mobileMenuLink.addEventListener('click', () => {
+    btn.classList.toggle('open');
+    menu.classList.toggle('hidden');
+  });
+});
